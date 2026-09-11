a23f2597a50e044661734fb5c54e5d739ea32f33
Abdulrahman Yasser <abdu.ya74@gmail.com>
2026-09-11 18:41:11 +0300

    making bo and writing into it

--- 11-drm/main.cpp
+++ 11-drm/main.cpp
@@ -4,11 +4,13 @@
 #include <fcntl.h>    // For open()
 #include <gbm.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
 using namespace std;
 
 
 int main(){
+    int height = 120, width = 120;
     int fd = open("/dev/dri/renderD128", O_RDWR | O_CLOEXEC);
     if(fd == -1){
         printf("Error openning the file \n");
@@ -44,18 +46,48 @@ int main(){
     printf("\nGBM_FORMAT_NV12, GBM_BO_USE_LINEAR : %d\n", supported);
 
 
-    gbm_bo* my_bo = gbm_bo_create(my_device, 120, 120, GBM_FORMAT_XRGB8888, GBM_BO_USE_LINEAR);
-    uint32_t my_stride=120 * 4;
-    uint32_t *map_data ;
-    gbm_bo_map(my_bo, 0, 0, 120, 120, GBM_BO_USE_LINEAR, &my_stride, (void**)&map_data);
-    printf("here\n");
-    gbm_bo_unmap(my_bo, (void*)map_data);
+    gbm_bo* my_bo = gbm_bo_create(my_device, width, height, GBM_FORMAT_XRGB8888, GBM_BO_USE_LINEAR);
+    uint32_t my_stride=0;
+    void *map_data = NULL;
 
-    int new_fd = gbm_bo_get_fd(my_bo);
+    void* map_handle = gbm_bo_map(my_bo, 0, 0, width, height, GBM_BO_TRANSFER_WRITE, &my_stride, &map_data);
+    
+    for (int y = 0; y < 120; y++) {
+        uint8_t *row = (uint8_t*)map_handle + y * my_stride;
+        row[0] = (uint8_t)y ;
+    }
 
-    mmap(new_fd, );
+    gbm_bo_unmap(my_bo, map_data);
 
+
+    int dma_fd = gbm_bo_get_fd(my_bo);
+
+    size_t buf_size = (size_t)my_stride * height;
+    void *check = mmap(nullptr, buf_size, PROT_READ, MAP_SHARED, dma_fd, 0);
+
+    if (dma_fd == -1){
+        printf("error fstat\n");
+    }
+
+    bool ok =true;
+    uint8_t *bytes = (uint8_t *)check;
+
+    for(int y = 0; y < height; y++){
+        if(bytes[y*my_stride] != y){
+            printf("THERE IS A MISMATCH !!!\n");
+            ok = false;
+            break;
+        }
+    }
+
+    if(ok){
+        printf("Everything went well !\n");
+    }
+
+    munmap(check, buf_size);
+    close(dma_fd);
+    gbm_bo_destroy(my_bo);
     gbm_device_destroy(my_device);
-
     close(fd);
+    return 0;
 }
